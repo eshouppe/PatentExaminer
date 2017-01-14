a4cbc11e4a23cb14fd79438e7a851a6ebfade89e
Ryan Carlton <rlc120304@gmail.com>
2017-01-14 12:11:19 -0500

    Built search api

---
+++ README.md
@@ -0,0 +1,23 @@
+# README #
+Demo for Venn Software
+Written by Ryan Carlton & Emily Shouppe
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+* Other community or team contact
--- server.py
+++ server.py
@@ -1,19 +1,19 @@
 #!flask/bin/python
-from flask import Flask, jsonify, current_app
+from flask import Flask, request, jsonify, current_app
 
 app = Flask(__name__, static_url_path='/public')
 
 #Fake data for now
-tasks = [
+fresults = [
     {
-        'id': 1,
-        'title': u'Buy groceries',
+        'docid': 1,
+        'title': u'Buy groceries in Whole Foods',
         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
         'done': False
     },
     {
-        'id': 2,
-        'title': u'Learn Python',
+        'docid': 2,
+        'title': u'Learn Python Apis today',
         'description': u'Need to find a good Python tutorial on the web', 
         'done': False
     }
@@ -24,9 +24,27 @@ def static_proxy(path):
   # send_static_file will guess the correct MIME type
   return app.send_static_file(path)
 
-@app.route('/venn/api/v1.0/search', methods=['GET'])
+@app.route('/venn/api/v1.0/search', methods=['POST'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    #TODO: Add calls to process incoming request ect
+    #TODO: Add call capable of return the json below
+
+    #Check to ensure we have a json search object passed to us
+    #And we have a search param
+    #if not Abort search with a 400 code
+    if not request.json or not 'search' in request.json:
+        abort(400)
+
+    #Create Response Obj
+    task = {
+        'id': '1230i4',
+        'numOfResults': 2,
+        'searchedString': request.json['search'],
+        'results': fresults,
+        'done': False
+    }
+    #Return Response to request from client(JS)
+    return jsonify({'task': task})
 
 #Kick off the server
 if __name__ == '__main__':
