65a0620b9ed4298ad0b3db5f93afda437cc16913
Emily Shouppe <eshouppe@gmail.com>
2017-01-14 18:47:09 -0500

    Hook processer to server

--- processor/search.py
+++ processor/search.py
@@ -1,6 +1,7 @@
 import json
 
 
+#class Search_and_Match(object):
 # function to create all possible ordered substrings from search string
 def create_search_substrings(full_string):
     full_str_as_list = full_string.split(' ')
@@ -8,8 +9,9 @@ def create_search_substrings(full_string):
     all_substr = [" ".join(full_str_as_list[i:j+1]) for i in range(full_str_len) for j in range(i, full_str_len)]
     return all_substr
 
-def search_json(filepath, search_strings):
-    with open(other_location) as json_data:
+def search_json(filepath, full_string):
+    search_strings = create_search_substrings(full_string)
+    with open(filepath) as json_data:
         full_corpus = json.load(json_data)
         matching_patent_nums = []
         matching_patent_abstracts = []
@@ -21,14 +23,10 @@ def search_json(filepath, search_strings):
                     matching_patent_abstracts.append(full_corpus[key])
                     break
         
-        return matching_patent_nums, matching_patent_abstracts
+        return matching_patent_nums, matching_patent_abstracts, search_strings
 
 
-full_search_string = "earth mining"
-search_terms = create_search_substrings(full_search_string)
-print("Search terms: {}".format(", ".join(search_terms)))
-
-json_location = "patent_abstracts.json"
-other_location = "patents.json"
-match_num, match_abstract = search_json(other_location, search_terms)
-print("Matching patents: {}".format(", ".join(match_num)))
+#full_search_string = "earth-boring"
+#json_location = "patents.json"
+#match_num, match_abstract, search_terms = search_json(json_location, full_search_string)
+#print("Matching patents: {}".format(", ".join(match_num)))
