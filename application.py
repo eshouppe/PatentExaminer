2c61d090ea319f44455b4a93bcc45f1835c80030
Emily Shouppe <eshouppe@gmail.com>
2017-01-15 15:05:31 -0500

    App & search can handle 1 or 2 searches

--- application.py
+++ application.py
@@ -8,22 +8,6 @@ application = Flask(__name__)
 #TODO: remove cors before deployment...use for development of UI
 CORS(application)
 
-#Fake data for now
-fresults = [
-    {
-        'docid': 1,
-        'title': u'Buy groceries in Whole Foods',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'docid': 2,
-        'title': u'Learn Python Apis today',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
 @application.route('/<path:path>')
 def static_proxy(path):
   # send_static_file will guess the correct MIME type
@@ -37,19 +21,23 @@ def get_tasks():
     #Check to ensure we have a json search object passed to us
     #And we have a search param
     #if not Abort search with a 400 code
-    if not request.json or not 'search' in request.json:
+    if not request.json or ('search1' not in request.json and 'search2' not in request.json):
         abort(400)
     
-    # Entry point to call search. Pass it the request.json['search'].
-    #  It will return the response (take the place of task below)
-    processor = Search_and_Match(request.json['search'], "processor/patents.json")
-    match_num, match_abstract, searched_terms = processor.search_json()
+    # Instantiate search class and pass json location as param
+    processor = Search_and_Match(json_filepath="processor/patents.json")
+    # Call class method to perform search. If only 1 search, search2 is empty string.
+    if len(request.json['search2']) > 0:
+        search_list = [request.json['search1'], request.json['search2']]
+    else:
+        search_list = [request.json['search1']]
+    match_num, searched_terms = processor.prepare_vars(raw_search_list=search_list)
 
     #Create Response Obj
     task = {
         'numMatchedPatents': len(match_num),
-        'searchedString': request.json['search'],
-        'matchingPatentNums': match_num,
+        'searchedString(s)': search_list,
+        'matchingPatentNums': ", ".join(list(match_num.keys())),
         'searchedTerms': ", ".join(searched_terms)
     }
     #Return Response to request from client(JS)
--- processor/search.py
+++ processor/search.py
@@ -2,39 +2,55 @@ import json
 
 
 class Search_and_Match(object):
-    def __init__(self, raw_search_string, json_filepath):
-        self.raw_search_string = raw_search_string
+    def __init__(self, json_filepath):
         self.json_filepath = json_filepath
+        self.match_patent_nums = {}
 
-    def create_search_substrings(self):
-        """function to create all possible ordered substrings from search string"""
-        full_str_as_list = self.raw_search_string.split(' ')
+    def create_search_substrings(self, raw_search_string):
+        """Method to create all possible ordered substrings from search string"""
+        full_str_as_list = raw_search_string.split(' ')
         full_str_len = len(full_str_as_list)
         all_substr = [" ".join(full_str_as_list[i:j+1]) for i in range(full_str_len) for j in range(i, full_str_len)]
         return all_substr
 
-    def search_json(self):
+    def search_json(self, search_term_list, search_num):
         """Call function to craft search terms. Perform search on all values in JSON.
         Exit search if any term found in value."""
-        search_strings = self.create_search_substrings()
+        for key in self.full_corpus:
+            for term in search_term_list:
+                if term in self.full_corpus[key] :
+                    val = self.match_patent_nums.get(key, None) # Check if patent already matched
+                    if val is None: # If not already matched, add patent num & search number
+                        self.match_patent_nums[key] = [[search_num]]
+                    else: # If already matched, append search number
+                        self.match_patent_nums[key][0].append(search_num)
+                    break
+
+    def prepare_vars(self, raw_search_list):
+        """Group method that calls search term creation method and search json method"""
+        search_string_one = self.create_search_substrings(raw_search_list[0])
+        search_terms = search_string_one
+        try:
+            search_string_two = self.create_search_substrings(raw_search_list[1])
+            search_terms.extend(search_string_two)
+        except IndexError:
+            pass
+
         with open(self.json_filepath) as json_data:
-            full_corpus = json.load(json_data)
-            matching_patent_nums = []
-            matching_patent_abstracts = []
-
-            for key in full_corpus:
-                for this_str in search_strings:
-                    if this_str in full_corpus[key]:
-                        matching_patent_nums.append(key)
-                        matching_patent_abstracts.append(full_corpus[key])
-                        break
-            
-            return matching_patent_nums, matching_patent_abstracts, search_strings
+            self.full_corpus = json.load(json_data)
+            self.search_json(search_string_one, search_num=1)
+            try:
+                self.search_json(search_string_two, search_num=2)
+            except NameError:
+                pass
+        
+        return self.match_patent_nums, search_terms
 
 
-# full_search_string = "earth mining data"
+# search1 = "earth mining data"
+# search2 = "drill bits"
 # json_location = "patents.json"
-# inst = Search_and_Match(full_search_string, json_location)
-# match_num, match_abstract, search_terms = inst.search_json()
-# print("Matching patents: {}".format(", ".join(match_num)))
-# print("Search terms: {}".format(", ".join(search_terms)))
+# inst = Search_and_Match(json_location)
+# match_num, search_terms = inst.prepare_vars([search1])
+# print("Matching patents: ", match_num)
+# print("Search terms: ", search_terms)
